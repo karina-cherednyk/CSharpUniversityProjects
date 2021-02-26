0c632f3a9f2d40fe41ca5398142cd7cef3a679ed
Karina Cherednyk <karina0cher@gmail.com>
2021-02-27 00:56:14 +0200

    Add project files.

---
+++ Budgets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budgets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
